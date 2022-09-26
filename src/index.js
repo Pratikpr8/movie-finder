8ad649774c53995d24e02ae8bebb3defe9af571e
Pratikpr8 <pratikpr.official8@gmail.com>
2022-09-26 15:23:23 +0545

    fixed title

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AppProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/movie-finder">
         <App />
       </BrowserRouter>
     </AppProvider>
